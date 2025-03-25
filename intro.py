e0db9613c0fd3ae7cd20de745a40edf04b75a61f
George Yu <george@think.digital>
2025-03-25 11:30:38 +1100

    Learnt basic building blocks and chaining pattern

---
+++ intro.py
@@ -0,0 +1,150 @@
+from dotenv import load_dotenv
+import json
+import os
+
+import requests
+from openai import OpenAI
+from pydantic import BaseModel, Field
+
+load_dotenv() 
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+# --------------------------------------------------------------
+# Define the tool (function) that we want to call
+# --------------------------------------------------------------
+
+def search_kb(question: str):
+    """
+    Load the whole knowledge base from the JSON file.
+    (This is a mock function for demonstration purposes, we don't search)
+    """
+    with open("kb.json", "r") as f:
+        return json.load(f)
+
+    
+
+def get_weather(latitude, longitude):
+    """This is a publically available API that returns the weather for a given location."""
+    response = requests.get(
+        f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+    )
+    data = response.json()
+    return data["current"]
+
+
+# --------------------------------------------------------------
+# Step 1: Call model with get_weather tool defined
+# --------------------------------------------------------------
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_weather",
+            "description": "Get current temperature for provided coordinates in celsius.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "latitude": {"type": "number"},
+                    "longitude": {"type": "number"},
+                },
+                "required": ["latitude", "longitude"],
+                "additionalProperties": False,
+            },
+            "strict": True,
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "search_kb",
+            "description": "Get the answer to the user's question from the knowledge base.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "question": {"type": "string"},
+                },
+                "required": ["question"],
+                "additionalProperties": False,
+            },
+            "strict": True,
+        },
+    }
+]
+
+
+system_prompt = "You are a helpful assistant that answers questions from the knowledge base about our e-commerce store or answer real time weather data."
+
+messages = [
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": "What is the weather like in Tokyo?"},
+]
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=messages,
+    tools=tools,
+)
+
+
+
+# --------------------------------------------------------------
+# Step 2: Model decides to call function(s)
+# --------------------------------------------------------------
+
+completion.model_dump()
+
+# --------------------------------------------------------------
+# Step 3: Execute get_weather function
+# --------------------------------------------------------------
+
+
+def call_function(name, args):
+    if name == "get_weather":
+        return get_weather(**args)
+    if name == "search_kb":
+        return search_kb(**args)
+
+
+for tool_call in completion.choices[0].message.tool_calls:
+    name = tool_call.function.name
+    args = json.loads(tool_call.function.arguments)
+    messages.append(completion.choices[0].message)
+
+    result = call_function(name, args)
+    messages.append(
+        {"role": "tool", "tool_call_id": tool_call.id, "content": json.dumps(result)}
+    )
+
+# --------------------------------------------------------------
+# Step 4: Supply result and call model again
+# --------------------------------------------------------------
+
+
+class WeatherResponse(BaseModel):
+    temperature: float = Field(
+        description="The current temperature in celsius for the given location."
+    )
+    response: str = Field(
+        description="A natural language response to the user's question."
+    )
+
+class KBResponse(BaseModel):
+    answer: str = Field(description="The answer to the user's question.")
+    source: int = Field(description="The record id of the answer.")
+
+
+completion_2 = client.beta.chat.completions.parse(
+    model="gpt-4o",
+    messages=messages,
+    tools=tools,
+    response_format=WeatherResponse,
+)
+
+# --------------------------------------------------------------
+# Step 5: Check model response
+# --------------------------------------------------------------
+
+final_response = completion_2.choices[0].message.parsed
+print(final_response)
